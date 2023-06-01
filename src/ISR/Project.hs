00874f0c57bce9657ec4250581a9f99a632f3e3b
LighghtEeloo <litiaeeloo@gmail.com>
2023-06-01 21:53:42 +0800

    impl: `^.`

--- src/ISR/Project.hs
+++ src/ISR/Project.hs
@@ -1,5 +1,6 @@
 module ISR.Project (topEntity) where
 
+import Clash.Annotations.TH
 import Clash.Prelude
 import qualified ISR.Mult as Mult
 
@@ -7,7 +8,9 @@ topEntity ::
   "clock" ::: Clock System ->
   "reset" ::: Reset System ->
   "enable" ::: Enable System ->
-  Signal System Mult.Input ->
-  Signal System Mult.Output
+  "input" ::: Signal System Mult.Input ->
+  "output" ::: Signal System Mult.Output
 topEntity clock reset en input =
   exposeClockResetEnable (Mult.mult input) clock reset en
+
+makeTopEntityWithName 'topEntity "ISR"
--- src/ISR/StagedMult.hs
+++ src/ISR/StagedMult.hs
@@ -1,93 +1,78 @@
-{-# LANGUAGE NamedFieldPuns #-}
-{-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE FlexibleContexts #-}
+-- {-# LANGUAGE NamedFieldPuns, RecordWildCards, DuplicateRecordFields #-}
 
-module ISR.StagedMult (Input, Output, ProdBundle) where
+module ISR.StagedMult (staged, Input, Output, ProdBundle) where
 
-import Clash.Prelude
+import Clash.Prelude hiding (init)
+import Control.Lens hiding (op)
 
-newtype Identity a = Identity { runIdentity :: a }
+-- import Control.Monad.RWS
+-- import Data.Monoid.Generic
 
-data ProdBundle t = ProdBundle
-  { prod :: t (Unsigned 64),
-    mplier :: t (Unsigned 64),
-    mcand :: t (Unsigned 64)
+data ProdBundle = ProdBundle
+  { _prod :: Unsigned 64,
+    _mplier :: Unsigned 64,
+    _mcand :: Unsigned 64
   }
-deriving instance Generic (ProdBundle t)
--- deriving instance Show (ProdBundle t)
--- deriving instance Eq (ProdBundle t)
--- deriving instance NFDataX (ProdBundle t)
-instance Bundle (ProdBundle Identity) where
-  type instance Unbundled dom (ProdBundle Identity) = ProdBundle (Signal dom)
-  bundle ProdBundle {prod,mplier,mcand} =
-    ProdBundle <$> (Identity <$> prod) <*> (Identity <$> mplier)<*> (Identity <$> mcand)
-    -- undefined
-  unbundle m =
-    ProdBundle
-      {
-        prod = runIdentity . prod <$> m,
-        mplier = runIdentity . mplier <$> m,
-        mcand = runIdentity . mcand <$> m
-      }
+  deriving stock (Generic, Show, Eq)
+  deriving anyclass (NFDataX)
 
-data Input t = Input
-  { start :: t Bool,
-    prods_in :: ProdBundle t
+makeLenses ''ProdBundle
+
+data Input = Input
+  { _start :: Bool,
+    _prods_in :: ProdBundle
   }
-deriving instance Generic (Input t)
--- deriving instance Show (Input t)
--- deriving instance Eq (Input t)
--- deriving instance NFDataX (Input t)
-instance Bundle (Input Identity) where
-  type instance Unbundled dom (Input Identity) = Input (Signal dom)
-  bundle Input {start,prods_in} =
-    Input <$> (Identity <$> start) <*> bundle prods_in
-    -- undefined
-  unbundle m =
-    Input
-      {
-        start = runIdentity . start <$> m,
-        prods_in = unbundle $ prods_in <$> m
-      }
+  deriving stock (Generic, Show, Eq)
+  deriving anyclass (NFDataX)
 
-data Output t = Output
-  { prods_out :: ProdBundle t,
-    done :: t Bool
+makeLenses ''Input
+
+data Output = Output
+  { _prods_out :: ProdBundle,
+    _done :: Bool
   }
-deriving instance Generic (Output t)
--- deriving instance Show (Output t)
--- deriving instance Eq (Output t)
--- deriving instance NFDataX (Output t)
+  deriving stock (Generic, Show, Eq)
+  deriving anyclass (NFDataX)
 
-instance Bundle (Output Identity) where
-  type instance Unbundled dom (Output Identity) = Output (Signal dom)
-  bundle Output {prods_out,done} =
-    Output <$> bundle prods_out <*> (Identity <$> done)
-  unbundle m =
-    Output
-      {
-        prods_out = unbundle $ prods_out <$> m,
-        done = runIdentity . done <$> m
+makeLenses ''Output
 
-      }
+data State = State
+  { _prod_s :: ProdBundle,
+    _partial :: Unsigned 64,
+    _done_s :: Bool
+  }
+  deriving stock (Generic, Show, Eq)
+  deriving anyclass (NFDataX)
 
+makeLenses ''State
 
-
--- staged :: (HiddenClockResetEnable dom) => Signal dom (Input Identity) -> Signal dom (Output Identity)
--- staged input =
---   bundle $ Output
---     { prods_out =
---         ProdBundle
---           { prod = partial + prod,
---             mplier = mplier `shiftR` width,
---             mcand = mcand `shiftL` width
---           },
---       done =
---         start
---     }
---   where
---     Input {start, prods_in} = unbundle input
---     start_ = start :: Input (Signal dom)
---     ProdBundle {prod, mplier, mcand} = prods_in
---     width = 8
---     partial = ((unpack $ zeroBits ++# slice d7 d0 mplier) :: Unsigned 64) * mcand
+staged :: (HiddenClockResetEnable dom) => Signal dom Input -> Signal dom Output
+staged = moore trans output init
+  where
+    trans (s :: State) (i :: Input) =
+      State
+        { _prod_s =
+            ProdBundle
+              { _prod = i ^. (prods_in . prod),
+                _mplier = i ^. (prods_in . mplier) `shiftR` 8,
+                _mcand = i ^. (prods_in . mcand) `shiftL` 8
+              },
+          _partial = s ^. partial,
+          _done_s = i ^. start
+        }
+    output (s :: State) =
+      Output
+        { _prods_out =
+            ProdBundle
+              { _prod = s ^. partial + s ^. (prod_s . prod),
+                _mplier = s ^. (prod_s . mplier),
+                _mcand = s ^. (prod_s . mcand)
+              },
+          _done = s ^. done_s
+        }
+    init :: State =
+      State
+        { _prod_s = ProdBundle 0 0 0,
+          _partial = 0,
+          _done_s = False
+        }
